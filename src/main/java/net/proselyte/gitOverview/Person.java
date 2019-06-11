609ed7ffbc5441f5551bef5b2c82e9694a56ea5b
Uktam Nimatov <uktam.nimatov@gmail.com>
2019-06-11 16:47:10 +0500

    Adding person class

---
+++ src/main/java/net/proselyte/gitOverview/Person.java
@@ -0,0 +1,33 @@
+package net.proselyte.gitOverview;
+
+public class Person extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Person setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Person setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}

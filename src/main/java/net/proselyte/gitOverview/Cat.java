e947c9dc112513dbd7c35b6e0063c975f0a2cf13
Uktam Nimatov <uktam.nimatov@gmail.com>
2019-06-11 17:05:37 +0500

    added new Cat class

---
+++ src/main/java/net/proselyte/gitOverview/Cat.java
@@ -0,0 +1,25 @@
+package net.proselyte.gitOverview;
+
+public class Cat {
+    private String name;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public Cat setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Cat setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+}
